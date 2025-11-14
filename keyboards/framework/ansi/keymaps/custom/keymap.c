cdb8456cc8b87c9e5067e43c208b60fd233cc886
Mike Lloyd <mike.lloyd03@pm.me>
2025-11-13 22:17:46 -0600

    Return to enter on alt

--- keyboards/framework/ansi/keymaps/custom/keymap.c
+++ keyboards/framework/ansi/keymaps/custom/keymap.c
@@ -40,12 +40,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * 78 total
      */
     [_BASE] = LAYOUT(
-        KC_ESC,  TD(1),   TD(2),   TD(3),   TD(4),   TD(5),   TD(6),   TD(7),   TD(8),   TD(9),   TD(10),  TD(11),  TD(12), KC_DEL,
+        KC_ESC,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_BRIU, KC_SCRN, KC_AIRP, KC_PSCR, KC_MSEL, KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         HYPER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LGUI, MO(_FN), KC_LALT, KC_LCTL,          SUPERSPACE,                KC_ENT,  KC_RCTL, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
+        KC_LGUI, MO(_FN), KC_LALT, KC_LCTL,          SUPERSPACE,                KC_RALT, KC_RCTL, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
     ),
      /*
      * Function layer
